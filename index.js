216026cd76697eba4701174a73e69985c0943ae1
Taiwo Lambo <lmb.taiwo@gmail.com>
2022-10-04 12:55:42 +0100

    added cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import {connectDB} from "./config/db.js"
 import recipeRoutes from "./routes/recipeRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
+import cors from "cors";
 
 
 dotenv.config()
@@ -11,6 +12,10 @@ connectDB()
 const app = express();
 
 app.use(express.json())
+
+app.use(cors({
+    origin: "*"
+}));
 
 const port = process.env.PORT || 8000;
 
